6f195d7cd6966005b94094e1a81d6a53c754d509
abdulkhaliqdev <abdulkhaliqdev@gmail.com>
2022-07-10 14:55:17 +0500

    Fix folder stucture

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import Header
-import Header from "./components/Header/Header";
-import Main from "./components/Main/main";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 const App = () => (
   <div>
